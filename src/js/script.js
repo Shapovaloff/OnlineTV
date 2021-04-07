5da1847137756b26b4dc9f0a2130ebf5d52cb31e
Yura <mr.Shapovaloff3101@gmail.com>
2021-04-08 01:47:13 +0300

    mobile version header

---
+++ src/js/script.js
@@ -0,0 +1,25 @@
+const btnMenu = document.querySelector('.js-btn-menu');
+const navMenu = document.querySelector('.js-nav-menu');
+const overlay = document.querySelector('.overlay');
+
+const closeMenu = () => {
+  navMenu.classList.remove('nav-list--active');
+  overlay.classList.remove('overlay--active');
+  btnMenu.classList.remove('header__btn-toggle--active');
+}
+
+const openMenu = () => {
+  navMenu.classList.add('nav-list--active');
+  overlay.classList.add('overlay--active');
+  btnMenu.classList.add('header__btn-toggle--active');
+  overlay.addEventListener('click', closeMenu);
+}
+
+if (btnMenu && navMenu) {
+  btnMenu.addEventListener('click', () => {
+    btnMenu.classList.contains('header__btn-toggle--active')
+      ? closeMenu()
+      : openMenu();
+  });
+  
+}
