cd8b4f872f14de8ff01ca4ffc2108b7e651f3301
Yura <mr.Shapovaloff3101@gmail.com>
2021-04-17 11:43:06 +0300

    firs screen

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+// Открытие и закрытие меню в мобильной версии
 const btnMenu = document.querySelector('.js-btn-menu');
 const navMenu = document.querySelector('.js-nav-menu');
 const overlay = document.querySelector('.overlay');
@@ -6,19 +7,19 @@ const closeMenu = () => {
   navMenu.classList.remove('nav-list--active');
   overlay.classList.remove('overlay--active');
   btnMenu.classList.remove('header__btn-toggle--active');
-}
+  document.body.style.overflow = 'auto';
+};
 
 const openMenu = () => {
   navMenu.classList.add('nav-list--active');
   overlay.classList.add('overlay--active');
   btnMenu.classList.add('header__btn-toggle--active');
   overlay.addEventListener('click', closeMenu);
-}
+  document.body.style.overflow = 'hidden';
+};
 
 if (btnMenu && navMenu) {
   btnMenu.addEventListener('click', () => {
-    btnMenu.classList.contains('header__btn-toggle--active')
-      ? closeMenu()
-      : openMenu();
+    btnMenu.classList.contains('header__btn-toggle--active') ? closeMenu() : openMenu();
   });
 }
